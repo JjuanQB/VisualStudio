eff306f1cebf7837f7d4443cc489306596fc3c74
Juan Quintero <juanquintero.jbe@gmail.com>
2024-10-30 15:54:05 -0500

    Primer Commit

---
+++ ProyPrueba001/Prueba001.c
@@ -0,0 +1,14 @@
+
+
+
+#include <stdio.h>
+#include <float.h>
+
+const long double valx = 3.14159265358979323846264338328;
+int main() {
+
+	double lfvar = valx;
+	printf("var= %.18Lf", lfvar);
+
+	return 0;
+}
