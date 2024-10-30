645e5d3ceab5faf9f683dc2cfe9881cd9a7cfb30
Juan Quintero <juanquintero.jbe@gmail.com>
2024-10-30 16:03:13 -0500

    Realización de nuevos cambios

--- ProyPrueba001/Prueba001.c
+++ ProyPrueba001/Prueba001.c
@@ -8,7 +8,8 @@ const long double valx = 3.14159265358979323846264338328;
 int main() {
 
 	double lfvar = valx;
-	printf("var= %.18Lf", lfvar);
+	printf("var= %.18Lf\n", lfvar);
+	printf("Cambios realzados\n");
 
 	return 0;
 }
